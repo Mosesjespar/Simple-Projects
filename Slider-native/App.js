a95e45ff643118b41d679e75e7ca237fea59cec0
Ssembatya Moses <104248822+Mosesjespar@users.noreply.github.com>
2022-07-29 22:21:45 +0300

    Create App.js

---
+++ Slider-native/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import Header from './components/RevivalPostsSlider'
+
+export default function App() {
+  return (
+    <View>
+      <Header />
+    </View>
+  )
+}
